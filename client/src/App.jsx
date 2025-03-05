cc6cfc61621a86c467fa1d63ab982c38bdf7a3af
Bastien <bpollet0@gmail.com>
2025-03-05 10:45:11 +0100

    page detailArticle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import react from 'react'
+import axios from "axios";
 import './App.css'
+import {Route, Link, Routes} from "react-router-dom"
 import Header from './components/Header'
+import Article from './components/Article.jsx'
+import Detail from './components/Detail.jsx'
+
+
 
 function App() {
   
 
   return (
     <>
-      <Header />
+       <Header />
+
+
+    <nav>
+      <Link to="/article">Article</Link>
+      <Link to="/detail/:id">Detail</Link>
+    </nav>
+
+    <Routes>
+      <Route path="/article" element={<Article />} />
+      <Route path="/detail/:id" element={<Detail />} />
+
+    </Routes>
+    
+ 
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Route, Link, Routes } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+
   </StrictMode>,
 )
