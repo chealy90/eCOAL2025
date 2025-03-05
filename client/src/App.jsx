46b2cdb6d343000be9bb2ef4a688069419af308e
louise_verstraete <louise_verstraete@ens.univ-artois.fr>
2025-03-05 10:28:55 +0100

    connection ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Login from './components/Login'
+import Register from './components/Register'
+import { useCookies } from 'react-cookie'
+import { Routes, Route, Link } from 'react-router'
 
 function App() {
-  
+  const [cookies, setCookie, removeCookie] = useCookies(['mycookie']);
 
   return (
     <>
-      <Header />
+      <Header/>
+      <Routes>
+        <Route path='/' element={<h2>cc</h2>}/>
+        <Route path="/login" element={<Login setCookie={setCookie}/>} />
+        <Route path="/register" element={<Register setCookie={setCookie}/>} />
+      </Routes>
     </>
   )
 }
