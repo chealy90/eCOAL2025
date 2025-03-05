fde49ababc99fc0bf231e16ac27d0d0f6fcf4d98
louise_verstraete <louise_verstraete@ens.univ-artois.fr>
2025-03-05 11:18:49 +0100

    css fix

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
 import axios from 'axios';
 import styles from './Login.module.css';
