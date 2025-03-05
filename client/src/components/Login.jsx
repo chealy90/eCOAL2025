518eeac7515e6e8ff3f3b10f8adbab1dd9436291
louise_verstraete <louise_verstraete@ens.univ-artois.fr>
2025-03-05 10:30:20 +0100

    connection ok 2

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router';
+import axios from 'axios';
+import styles from './Login.module.css';
+import { useLocation, useNavigate, useParams } from 'react-router';
+
+
+function Login(props) {
+    
+    
+    console.log(props.setCookie)
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const result = await axios.post('http://localhost:8000/api/login', {
+            email: email,
+            password: password
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+
+        if(result.status == 200) {
+            props.setCookie("mycookie", {name: result.data.name, token: result.data.token}, "/")
+            console.log(result)
+        }
+
+
+    }
+
+    return (
+        <div className={styles.login}>
+            <h1>Log in</h1>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input type="text" name="email" placeholder='Email' />
+                <input type="password" name="password" placeholder='Password' />
+                <button type="submit">Log in</button>
+            </form>
+            <p>Don't have an account? <Link to={"/register"}>Sign up</Link></p>
+        </div>
+    )
+}
+
+export default Login;
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router";
+import axios from "axios";
+import styles from "./Register.module.css"
+
+function Register(props) {
+
+    console.log(props.setCookie)
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const password2 = e.target.password2.value;
+
+        if (password !== password2) {
+            alert("Passwords do not match");
+        }
+
+        const result = await axios.post('http://localhost:8000/api/register', {
+            name: name,
+            email: email,
+            password: password
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        
+        if(result.status == 200) {
+            props.setCookie("mycookie", {name: result.data.name, token: result.data.token}, "/")
+            console.log(result)
+        }
+    }
+
+
+    return (
+        <div className={styles.register}>
+            <h1>Sign up</h1>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input type="text" name="name" placeholder='Name' />
+                <input type="text" name="email" placeholder='Email' />
+                <input type="password" name="password" placeholder='Password' />
+                <input type="password" name="password2" placeholder='Confirm password' />
+                <button type="submit">Sign up</button>
+            </form>
+            <p>Already have an account? <Link to={"/login"}>Log in</Link></p>
+        </div>
+    )
+}
+
+export default Register;
