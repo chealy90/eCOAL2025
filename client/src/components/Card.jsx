1ae4f7f2bcb492d4f6e72b3a171d14178e0141e2
Bourgain Lou <bourgainlou.mmi@gmail.com>
2025-03-05 10:52:43 +0100

    Modifications

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -44,7 +44,7 @@ const ExpandMore = styled((props) => {
 
 // NOTE: When the css is finally finished, we'll apply 'props' and replace the
 // hardcoded information, for example: description, title, author, etc;
-export default function Post() {
+export default function Post(props) {
     const [expanded, setExpanded] = React.useState(false); // State to manage ExpandMore button direction 
     const [favorite, setFavorite] = React.useState(false); // State to manage Favorite button color
 
@@ -72,12 +72,6 @@ export default function Post() {
                 title="Shrimp and Chorizo Paella"
                 subheader="September 14, 2016"
             />
-            <CardMedia
-                component="img"
-                height="194"
-                image="/static/images/cards/paella.jpg"
-                alt="Paella dish"
-            />
             <CardContent>
                 <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                     This impressive paella is a perfect party dish and a fun meal to cook
@@ -96,44 +90,8 @@ export default function Post() {
                 <IconButton aria-label="share">
                     <ShareIcon />
                 </IconButton>
-                <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </ExpandMore>
+                
             </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
-                    <Typography sx={{ marginBottom: 2 }}>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                        aside for 10 minutes.
-                    </Typography>
-                    <Typography sx={{ marginBottom: 2 }}>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                        medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                        occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                        large plate and set aside, leaving chicken and chorizo in the pan. Add
-                        pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                        stirring often until thickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography sx={{ marginBottom: 2 }}>
-                        Add rice and stir very gently to distribute. Top with artichokes and
-                        peppers, and cook without stirring, until most of the liquid is absorbed,
-                        15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                        mussels, tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                    </Typography>
-                </CardContent>
-            </Collapse>
         </Card >
     );
 }
