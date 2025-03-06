6297995651d8fc60f13425348c7db388e6e2295a
Bastien <bpollet0@gmail.com>
2025-03-06 10:38:21 +0100

    import icons

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -8,6 +8,10 @@ import { useParams } from 'react-router';
 
 
 
+
+
+
+
 function Detail() {
 
     let params = useParams();
@@ -20,17 +24,17 @@ function Detail() {
 
     useEffect(() => {
         getArticle()
-    },[])
+    }, [])
 
-    if (article1 === null) 
+    if (article1 === null)
         return (<div>loading...</div>)
- 
+
     return (
-        <div className={styles.detailArticle}>  
+        <div className={styles.detailArticle}>
             <div className={styles.blockTitle}>
                 <h1 className={styles.articleH1}>{article1.title}</h1>
             </div>
-            <p className={styles.articleContent}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit recusandae facere id sit quam earum quos consequatur soluta maiores consequuntur optio cupiditate fugiat blanditiis veritatis molestias, possimus cum aut incidunt.</p>         
+            <p className={styles.articleContent}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit recusandae facere id sit quam earum quos consequatur soluta maiores consequuntur optio cupiditate fugiat blanditiis veritatis molestias, possimus cum aut incidunt.</p>
             <img className={styles.imgArticle} src="https://www.photobox.fr/content/blog/uploads/2017/06/scene-de-concert-foule-lumieres.jpg?w=1800" />
         </div>
     )
